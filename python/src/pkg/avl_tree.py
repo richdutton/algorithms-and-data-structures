88d306ab3c9d42fd34a578ae07cb822bb8a59003
Richard Dutton <github@richdutton.org.uk>
2017-03-21 14:33:39 +0000

    removed insertion point member; removed insertions; cleaned up function ordering and names

--- python/src/pkg/avl_tree.py
+++ python/src/pkg/avl_tree.py
@@ -8,86 +8,91 @@ InsertionPoint = Enum('InsertionPoint', '_left _right root')
 _REBALANCE = True
 
 
-# TODO: rather than keeping reference to parent would it be easier to check balance of child rather than self?
-#
 class Node:
-    def __init__(self, key, parent, insertion_point, do_not_rebalance):
+    def __init__(self, key, parent, do_not_rebalance):
         self._key = key
         self._left = None
         self._right = None
         self._parent = parent
-        self._insertion_point = insertion_point
         self._do_not_rebalance = do_not_rebalance
 
         self._balance_factor = 0
         self._height = 1
 
-    def _insert_key(self, key):
-        assert key != self._key
+    def _update_height_and_balance_factor(self):
+        left_height = self._node_height(self._left)
+        right_height = self._node_height(self._right)
 
-        insertion_point = InsertionPoint._left if key < self._key else InsertionPoint._right
-        insertions = [insertion_point]
+        self._height = 1 + max(left_height, right_height)
+        self._balance_factor = left_height - right_height
 
-        if getattr(self, insertion_point.name) is None:
-            setattr(self, insertion_point.name, Node(key, self, insertion_point, self._do_not_rebalance))
-        else:
-            insertions.extend(getattr(self, insertion_point.name).insert(key))
+        return abs(self._balance_factor) > 1
 
-        return insertions
+    @property
+    def _insertion_point(self):
+        return (InsertionPoint.root if isinstance(self._parent, AVLTree)
+                else (InsertionPoint._left if self._parent._left is self else InsertionPoint._right))
 
-    def _right_rotation(self):
+    def _rotate_right(self):
         old_left = self._left
         self._left._right, self._left = self, self._left._right  # 1a,b
         if self._left is not None:
             self._left.parent = self
-            self._left._insertion_point = InsertionPoint._left
 
         setattr(self._parent, self._insertion_point.name, old_left)  # 2
-        old_left._insertion_point = self._insertion_point
         old_left._parent = self._parent  # 3
-
         self._parent = old_left  # 4
-        self._insertion_point = InsertionPoint._right
 
+        # TODO: could this be moved to _rotate or would that break double rotations?
         self._update_height_and_balance_factor()
 
-    # TODO: augment with new logic in right rotation when have tested and clarified same
-    def _left_rotation(self):
+    def _rotate_left(self):
         assert self._right._right is None
-        self._right._left = self
-        setattr(self._parent, self._insertion_point.name, self._right)
+        # self._right._left = self
+        # setattr(self._parent, self._insertion_point.name, self._right)
 
-    def _rotation(self, insertions):
-        assert len(insertions) >= 2
+    @staticmethod
+    def _node_height(node):
+        return node._height if node is not None else 0
 
-        # TODO: we've already checked balance factor but we're going to have to check it again here
-        # if height(node.left) >= 2 + height(node.right):
-        if insertions[0] == InsertionPoint._left:
-            # if height(node.left.left) >= height(node.left.right):
-            if insertions[1] == InsertionPoint._left:
-                self._right_rotation()
+    def _rotate(self):
+        # TODO: we've already checked balance factor but we're going to have to check it again here. is that optimal?
+        if self._node_height(self._left) - self._node_height(self._right) > 1:
+            # self._left must not be none given above
+            assert self._left is not None
+            if self._node_height(self._left._left) >= self._node_height(self._left._right):
+                self._rotate_right()
             else:
                 self._left._left_rotation()
-                self._right_rotation()
-        else:  # elif height(node.right) >= 2 + height(node.left):
-            # if height(node.right.right) >= height(node.right.left):
-            if insertions[1] == InsertionPoint._right:
-                self._left_rotation()
+                self._rotate_right()
+        else:
+            # if we need to rebalance then either left is overweight or right. it's not left per above
+            assert self._right is not None and self._node_height(self._right) - self._node_height(self._left) > 1
+            if self._node_height(self._right._right) >= self._node_height(self._right._left):
+                self._rotate_left()
             else:
                 self._right._right_rotation()
-                self._left_rotation()
+                self._rotate_left()
 
-    def _rebalance(self, insertions):
+    def _rebalance(self):
         if self._update_height_and_balance_factor() and not self._do_not_rebalance:
             AVLTree.rebalanced = True
-            self._rotation(insertions)
-            self._parent._rebalance(insertions)  # TODO: 0% sure insertions is valid at this stage
+            self._rotate()
+            # always recurse to ensure height and b.f. are updated (even though recursive rotations unnecessary for insert)
+            self._parent._rebalance()
+
+    def _insert_key(self, key):
+        assert key != self._key
+
+        insertion_point = InsertionPoint._left if key < self._key else InsertionPoint._right
+        if getattr(self, insertion_point.name) is None:
+            setattr(self, insertion_point.name, Node(key, self, self._do_not_rebalance))
+        else:
+            getattr(self, insertion_point.name).insert(key)
 
     def insert(self, key):
-        insertions = self._insert_key(key)
-        self._rebalance(insertions)
-
-        return insertions
+        self._insert_key(key)
+        self._rebalance()
 
     def __contains__(self, key):
         if key == self._key:
@@ -97,22 +102,13 @@ class Node:
         else:
             return False if self._right is None else key in self._right
 
-    def _update_height_and_balance_factor(self):
-        left_height = self._left._height if self._left is not None else 0
-        right_height = self._right._height if self._right is not None else 0
 
-        self._height = 1 + max(left_height, right_height)
-        self._balance_factor = left_height - right_height
-
-        return abs(self._balance_factor) > 1
-
-
-# TODO: move away from InsertionPoint in favor of identity check
-#       lose the insertion point / insertions
-#       implement _left_rotation
+# TODO: implement _left_rotation
+#        test double rotation
 #       remove do_not_rebalance hack in favor of monkeypatching or just careful construction of trees
 #       AVLTree.rebalanced hack (is there a better way?)
 #       potentially move most of logic from node into tree
+#       rather than keeping reference to parent would it be easier to check balance of child rather than self?#
 #
 class AVLTree:
     rebalanced = False
@@ -127,7 +123,7 @@ class AVLTree:
         self.rebalanced = False
 
         if not self.root:
-            self.root = Node(key, self, InsertionPoint.root, not self._rebalance)
+            self.root = Node(key, self, not self._rebalance)
         else:
             self.root.insert(key)
 
